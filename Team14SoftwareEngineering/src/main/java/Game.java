3ddd5b58f3067b6d610ac7d16ea1545957b05578
Christopher Carter <cicarter@uark.edu>
2022-02-27 19:41:28 -0600

    Commited as me

--- Team14SoftwareEngineering/src/main/java/Game.java
+++ Team14SoftwareEngineering/src/main/java/Game.java
@@ -10,40 +10,7 @@ public class Game {
 		System.out.println("Here at least");
 		playerDatabase.printTable();
 	}
-	
-	public void serverRestart()
-	{
-		/* 
-		 * At server restart I want to:
-		 * 		reconfigure database so that all players in the database have an ID that was removed will be reallocated to another player
-		 * 
-		 * 		IE: 
-		 * 		Original Database 
-		 * 		(
-		 * 			ID	Name	
-		 * 			0 	Chris
-		 * 			1	Mike
-		 * 			2	Tom
-		 * 			
-		 * 		)
-		 * 
-		 * 		Player Mike is removed
-		 * 
-		 * 		Database before restart
-		 * 		(
-		 * 			ID	Name	
-		 * 			0 	Chris
-		 * 			2	Tom
-		 * 			
-		 * 		)
-		 * 		Database After Restart
-		 * 		(
-		 * 			ID	Name	
-		 * 			0 	Chris
-		 * 			1	Tom
-		 * 		)
-		*/
-	}
+
 	
 	
 	
